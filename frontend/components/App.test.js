ae05fcb3090d0b1f849f4ac083c418e7dd4b300d
Camden Hess <camden.hess12@gmail.com>
2024-01-22 18:37:55 -0700

    finished module project

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,33 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import server from '../../backend/mock-server'
+import React from 'react'
+import AppFunctional from "./AppFunctional"
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('AppFunctional component', () => {
+  let emailBox = document.querySelector('#email')
+
+  beforeAll(() => { server.listen() })
+  afterAll(() => { server.close() })
+  beforeEach(() => {
+    render(<AppFunctional />)
+  })
+
+  test('UP', () => {
+    expect(screen.getByText('UP')).toBeVisible()
+  })
+
+  test('DOWN', () => {
+    expect(screen.getByText('DOWN')).toBeVisible()
+  })
+
+  test('RIGHT', () => {
+    expect(screen.getByText('RIGHT')).toBeVisible()
+  })
+
+  test('LEFT', () => {
+    expect(screen.getByText('LEFT')).toBeVisible()
+  })
 })
+
